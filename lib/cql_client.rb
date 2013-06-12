b3981881698ecdcbcc9e329de1ee762d2eb06a05
Brent Theisen <brent@bantamlabs.com>
2013-06-12 14:29:35 -0500

    Don't warn on dupe require.

--- lib/cql_client.rb
+++ lib/cql_client.rb
@@ -1,10 +1,10 @@
 # encoding: UTF-8
+require 'cql_client/version' unless defined? CqlClient::VERSION
 require 'cql_client/error'
 require 'cql_client/cql_client'
 require 'cql_client/connection'
 require 'cql_client/host'
 require 'cql_client/session'
-require 'cql_client/version'
 
 
 module CqlClient
